671a4cbd764730d888f3fe0d906d6a059b3dc927
Franklin Alex Aguilar Jaico <frankaj.2004@gmail.com>
2025-07-03 23:28:30 -0500

    Primer commit

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import './App.css';
 function App() {
   return (
     <RecipeProvider>
+      {/*Configuración de rutas con React Router*/}
       <BrowserRouter>
         <div className="app">
           <Navbar />
